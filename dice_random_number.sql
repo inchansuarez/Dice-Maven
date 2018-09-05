f4701ac78695a999adf7339a5d3870cff6209073
Your Name <you@example.com>
2018-09-05 10:56:59 +0800

    fix sql

--- dice_random_number.sql
+++ dice_random_number.sql
@@ -1,3 +1,4 @@
+CREATE DATABASE  IF NOT EXISTS `dice` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
 -- MySQL dump 10.13  Distrib 8.0.12, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: dice
